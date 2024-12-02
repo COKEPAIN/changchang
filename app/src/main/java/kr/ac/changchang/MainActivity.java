55f1d6fb01c278b7ee26500d8b18a9fea0e22d9e
USER <yyrr0201@naver.com>
2024-12-02 22:19:21 +0900

    홈 액티비티에서 다른 액티비티로 전환하는 기능 구현 manifest파일에 다른액티비티 경로 추가함

--- app/src/main/java/kr/ac/changchang/MainActivity.java
+++ app/src/main/java/kr/ac/changchang/MainActivity.java
@@ -1,6 +1,9 @@
 package kr.ac.changchang;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +12,57 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton home, map, todo, shop, profile, book;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);testtest
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
+
+        home = (ImageButton) findViewById(R.id.btn_home);
+        map = (ImageButton) findViewById(R.id.btn_map);
+        todo = (ImageButton) findViewById(R.id.btn_todo);
+        shop = (ImageButton) findViewById(R.id.btn_shop);
+        profile = (ImageButton) findViewById(R.id.btn_profile);
+        book = (ImageButton) findViewById(R.id.btn_drawingbook);
+
+        map.setOnClickListener(new View.OnClickListener() { // 맵 액티비티 전환
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getApplicationContext(),Map.class);
+                startActivity(intent);
+            }
+        });
+        profile.setOnClickListener(new View.OnClickListener() { // 프로필 액티비티 전환
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getApplicationContext(),Profile.class);
+                startActivity(intent);
+            }
+        });
+        todo.setOnClickListener(new View.OnClickListener() { // todo list 액티비티 전환
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getApplicationContext(),Todo.class);
+                startActivity(intent);
+            }
+        });
+        book.setOnClickListener(new View.OnClickListener() { // 도감 액티비티 전환
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getApplicationContext(),Book.class);
+                startActivity(intent);
+            }
+        });
+        shop.setOnClickListener(new View.OnClickListener() { // 상점 액티비티 전환
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getApplicationContext(),Shop.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
