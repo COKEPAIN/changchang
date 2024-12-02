f4754e5d245a10414cd3ce420e1ccf9dada56e36
USER <yyrr0201@naver.com>
2024-12-02 18:22:19 +0900

    test push

--- app/src/main/java/kr/ac/changchang/MainActivity.java
+++ app/src/main/java/kr/ac/changchang/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);test
+//        EdgeToEdge.enable(this);testtest
         setContentView(R.layout.activity_home);
     }
 }
