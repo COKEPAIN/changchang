243aa698104ff1d18749249c3b06c6733362ff02
신승민 <tlstmdals05@gmail.com>
2024-12-04 19:19:13 +0900

    신승민_배경음악 추가 및 설정 변경

--- app/src/main/java/kr/ac/changchang/MainActivity.java
+++ app/src/main/java/kr/ac/changchang/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 승민 - 배경음악
+    private long backBtnTime = 0;  // 클래스 상단에 멤버 변수로 선언
+
     ImageButton home, map, todo, shop, profile, book;
     ImageView changchang;
     Intent intent;
@@ -23,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // 승민
+        startService(new Intent(getApplicationContext(), MusicService.class));
+
+
 
         home = (ImageButton) findViewById(R.id.btn_home);
         map = (ImageButton) findViewById(R.id.btn_map);
@@ -39,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 intent = new Intent(getApplicationContext(),Map.class);
+                // 승민
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_USER_ACTION);
                 startActivity(intent);
             }
         });
@@ -46,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 intent = new Intent(getApplicationContext(),Profile.class);
+                // 승민
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_USER_ACTION);
                 startActivity(intent);
             }
         });
@@ -53,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 intent = new Intent(getApplicationContext(),Todo.class);
+                // 승민
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_USER_ACTION);
                 startActivity(intent);
             }
         });
@@ -60,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 intent = new Intent(getApplicationContext(),Book.class);
+                // 승민
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_USER_ACTION);
                 startActivity(intent);
             }
         });
@@ -67,6 +85,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 intent = new Intent(getApplicationContext(),Shop.class);
+                // 승민
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_USER_ACTION);
                 startActivity(intent);
             }
         });
@@ -77,5 +97,36 @@ public class MainActivity extends AppCompatActivity {
                 changsay.setText("test");
             }
         });
+    }
+
+    // 승민 - 배경음악
+    @Override
+    protected void onDestroy() {
+        stopService(new Intent(getApplicationContext(), MusicService.class));
+        super.onDestroy();
+    }
+
+    // 승민 - 홈버튼으로 앱 나가면 음악 꺼지게
+    @Override
+    protected void onUserLeaveHint() {
+        super.onUserLeaveHint();
+
+        stopService(new Intent(getApplicationContext(), MusicService.class));
+    }
+
+    // 승민 - 뒤로가기 두 번 이벤트
+    @Override
+    public void onBackPressed(){
+        long curTime = System.currentTimeMillis();
+        long gapTime = curTime - backBtnTime;
+
+        if ( 0 <= gapTime && 2000 >= gapTime) {
+            super.onBackPressed();
+
+        } else {
+            backBtnTime = curTime;
+            Toast.makeText(this, "한 번더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+        }
+        stopService(new Intent(getApplicationContext(), MusicService.class));
     }
 }
